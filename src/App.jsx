9b08dd9a95f2da5e77a4cb3e2ef28bac1ccdeda8
JaveriyaSaleem <javeriyasaleem678@gmail.com>
2025-01-07 13:00:40 +0500

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,40 @@
+import React from 'react'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+
+const App = () => {
+const [input, setinput] = useState('')
+const [items, setitems] = useState([])
+const handelItem = ()=>{
+  if(input.trim()){
+    let itemsCopy =[...items,input]
+    setitems(itemsCopy)
+  
+  console.log(itemsCopy) 
+  setinput('')
+  }
+  else{
+    alert("Can't add empty")
+  }
+
+}
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+    
+      <h1 style={{textAlign:"center"}}>Items List</h1>
+      <div style={{display:"flex",flexDirection: "row", gap: "2px"}}>
+   <input type="text" value={input} onChange={(e)=>{setinput(e.target.value);}}/>
+   <button onClick={handelItem}>Add Item</button>
+   </div>
+   <ul>
+    {items.map((item,index)=>(
+     
+    <li key={index}>{item}</li>
+    ))}
+   </ul>
+    </div>
   )
 }
 
